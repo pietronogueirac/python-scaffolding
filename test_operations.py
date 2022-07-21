13d888bf74d732ac8ba3276b6e235831ea6e56f8
Pietro Calixto <pietrocalixto@MacBook-Pro-de-Pietro.local>
2022-07-21 15:53:31 -0300

    consertando as linhas de código again

--- test_operations.py
+++ test_operations.py
@@ -4,7 +4,7 @@ def test_operacoes(x=5):
     
     var1 = operations.potencialidade(x)
     var = operations.raiz_quadrada(var1)
-    #assert x == var
+    assert x == var
 
 def test_operacoes_2(x=5,y=2):
 
@@ -21,8 +21,3 @@ def test_operacoes_3(x=10,y=4):
     var1 = operations.divisão(var,y)
 
     assert x == var1
-
-if __name__ == "__main__":
-    test_operacoes(5)
-    test_operacoes_2(5,2)
-    test_operacoes_2(30,40)
